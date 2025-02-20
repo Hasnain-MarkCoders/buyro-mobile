c6456bc1e2bd4d77e371b61c3c844f76485e8909
Hasnain-MarkCoders <hasnain.raza@markcoders.com>
2025-02-20 10:57:22 +0500

    splash screen added

--- App.jsx
+++ App.jsx
@@ -3,8 +3,13 @@ import {
   QueryClientProvider,
 } from '@tanstack/react-query'
 import AppRoutes from "./AppRoutes";
+import SplashScreen from 'react-native-splash-screen'
+import { useEffect } from 'react';
 const queryClient = new QueryClient()
 function App(){
+  useEffect(()=>{
+    SplashScreen.hide();
+  },[])
   return(
  <>
   <QueryClientProvider client={queryClient}>
--- android/app/src/main/java/com/buyro/MainActivity.kt
+++ android/app/src/main/java/com/buyro/MainActivity.kt
@@ -1,13 +1,15 @@
 package com.buyro
-import android.os.Bundle;
+
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
-
+import org.devio.rn.splashscreen.SplashScreen;
+import android.os.Bundle;
 class MainActivity : ReactActivity() {
    override fun onCreate(savedInstanceState: Bundle?) {
+    SplashScreen.show(this)
     super.onCreate(null)
   }
 
@@ -22,5 +24,6 @@ class MainActivity : ReactActivity() {
    * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
    */
   override fun createReactActivityDelegate(): ReactActivityDelegate =
+  
       DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
 }
